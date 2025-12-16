88b439ffb6ce03296c83b7c921daefe4a4664e74
kuangyue111 <137126555+kuangyue111@users.noreply.github.com>
2025-12-16 16:16:50 +0800

    version_patch.h
    version_patch.h

---
+++ version_patch.h
@@ -0,0 +1,5 @@
+
+//added by Jimmy 20230524 for new verion mangement patch number
+
+#define SWVERSION_PATCH_NUMBER  ""        //main version, no patch
+//#define SWVERSION_PATCH_NUMBER  "T1"    //"" means no patch, patch should be from 1
